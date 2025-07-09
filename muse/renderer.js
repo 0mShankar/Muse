9fdd17c4f355e28fcb426e97f3040a4d15f422e4
0mShankar <omshankar9798@gmail.com>
2025-07-09 18:07:32 +0530

    welcome screen, button's

--- muse/renderer.js
+++ muse/renderer.js
@@ -1,10 +1,30 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const fileNameDisplay=document.getElementById('fileName');
+  const fileButton = document.getElementById('customFileButton');
   const fileInput = document.getElementById('fileInput');
   const video = document.getElementById('videoPlayer');
   const audio = document.getElementById('audioPlayer');
+  const enterAppBtn = document.getElementById('enterApp');
+const welcomeScreen = document.getElementById('welcomeScreen');
+const appContent = document.getElementById('appContent');
+
+
+enterAppBtn.addEventListener('click', () => {
+  welcomeScreen.style.display = 'none';
+  appContent.style.display = 'block';
+});
+
+
+
+
+  fileButton.addEventListener('click', () => {
+    fileInput.click();
+  });
+
 
   fileInput.addEventListener('change', (event) => {
     const file = event.target.files[0];
+    fileNameDisplay.textContent=file?file.name:"No File Selected";
     const url = URL.createObjectURL(file);
 
     if (file.type.startsWith('video/')) {
